63b7000c497cf1f7773aab9e07af2bbdb3564072
rashedmern <rashedmolla541@gmail.com>
2026-03-14 15:12:40 +0600

    document added

---
+++ script.js
@@ -0,0 +1,12 @@
+// function addTwoNumbers (a, b){
+//     let sum = a + b;
+//     return sum;
+// }
+//  const sum = addTwoNumbers(5, 10);
+//  console.log("sum :", sum)
+
+function loginUsermessage(username){
+     
+    return `welcome back ${username}`;
+}   
+ console.log(loginUsermessage("Faizaaa"));

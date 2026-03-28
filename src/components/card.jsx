af1bf12d2bcecd9bddd442e56fa67dda9104cbba
rashedmern <rashedmolla541@gmail.com>
2026-03-29 00:47:19 +0600

    components

---
+++ src/components/card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+  return (
+    <div className="bg-pink-400 text-white p-5 rounded-[30px] shadow-lg mt-10">
+      <h1>Rashed</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  )
+}
+export default Card
